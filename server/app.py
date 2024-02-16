a477092c2682213cd28e8481c9179cf742ce4d47
ckorley4 <ckorley4uo@yahoo.com>
2024-02-16 18:12:09 -0500

    initial commit

--- server/app.py
+++ server/app.py
@@ -2,8 +2,10 @@ from config import app, migrate
 
 from models import db
 
+def welcome_screen():
+    print("Hello World!")
 if __name__ == "__main__":
   with app.app_context():
     migrate.init_app(app, db)
-    pass
+    welcome_screen()
     # remove pass and write your cli logic
