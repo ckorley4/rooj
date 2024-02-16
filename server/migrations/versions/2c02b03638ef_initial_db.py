a5f81c94a85bd9205e0f85be85ed424ba010e966
ckorley4 <ckorley4uo@yahoo.com>
2024-02-16 18:07:48 -0500

    initial commit

---
+++ server/migrations/versions/2c02b03638ef_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 2c02b03638ef
+Revises: 
+Create Date: 2024-02-16 18:07:46.857735
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2c02b03638ef'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
